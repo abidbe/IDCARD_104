f035e700b5379d3b66735541dffb08cc34413307
abidbe <abid.i.ft20@mail.umy.ac.id>
2022-11-15 17:26:03 +0700

    step 2

---
+++ src/main/java/ws/c/idcard/controller.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ws.c.idcard;
+
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import org.apache.tomcat.util.codec.binary.Base64;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+
+/**
+ *
+ * @author asus
+ */
+@Controller
+public class controller {
+    @ResponseBody
+    @RequestMapping ("/getData")
+    public String getDate(@RequestParam("nama")String text,
+                          @RequestParam("image")MultipartFile file,
+                          @RequestParam("tanggal")@DateTimeFormat(pattern="yyyy-MM-dd")Date date)
+                          throws IOException{ 
+        SimpleDateFormat tanggal = new SimpleDateFormat("EE, dd MMMM yyyy");
+        String newTanggal = tanggal.format(date);
+        
+        String blob = Base64.encodeBase64String(file.getBytes());
+        
+       return text+"<br><img width=100 src='data:image/jpeg;base64,"+blob+"'/><br>"+newTanggal;
+    }
+}
